206ee2583fb346533c52476d9dc72124f9b56f9b
DeepBindal <deepbindal36@gmail.com>
2026-03-30 10:47:05 +0530

    added company name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,6 +51,7 @@ export default function Navbar() {
             aria-label="Go to homepage"
           >
             <img src="/hairport_logo.png" alt="logo" className="h-18 w-24"/>
+            <p>The Hairport Saloon</p>
           </Link>
         </div>
 
